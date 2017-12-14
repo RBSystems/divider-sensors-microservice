03f7b6fba79ac37efb06b2742a971f407b51f692
John Werner <jwerner547@gmail.com>
2017-12-14 14:54:21 -0700

    Added to script to prompt editing of config.json to correct values

--- script.sh
+++ script.sh
@@ -5,6 +5,8 @@ echo Enter the DSP microservice address for these sensors: read DSPaddress
 mkdir /etc/divider-sensors
 
 curl https://raw.githubusercontent.com/byuoitav/divider-sensors-microservice/feature/deployment/config.json > /etc/divider-sensors/
+vim /etc/divider-sensors/config.json
+
 curl https://raw.githubusercontent.com/byuoitav/divider-sensors-microservice/feature/deployment/divider-sensors.service > /etc/divider-sensors/
 curl https://github.com/byuoitav/divider-sensors-microservice/releases/download/v0.1/divider-sensors-microservice > /etc/divider-sensors/
 
