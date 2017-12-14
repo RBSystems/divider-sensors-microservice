7f934c0c190063f7e127208bcf1df8b47e6564cc
John Werner <jwerner547@gmail.com>
2017-12-14 14:52:01 -0700

    changed config file to default values, wrote script to test setup of a DS pi

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+echo Enter the DSP microservice address for these sensors: read DSPaddress
+"DSP_MICROSERVICE_ADDRESS=$DSPaddress" >> /etc/environment
+
+mkdir /etc/divider-sensors
+
+curl https://raw.githubusercontent.com/byuoitav/divider-sensors-microservice/feature/deployment/config.json > /etc/divider-sensors/
+curl https://raw.githubusercontent.com/byuoitav/divider-sensors-microservice/feature/deployment/divider-sensors.service > /etc/divider-sensors/
+curl https://github.com/byuoitav/divider-sensors-microservice/releases/download/v0.1/divider-sensors-microservice > /etc/divider-sensors/
+
+systemctl enable divider-sensors.service
