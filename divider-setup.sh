a4276f4ec81cbe10e764c1d0d1c160faa6f100d5
John Werner <jwerner547@gmail.com>
2018-01-17 09:42:57 -0700

    made some minor tweaks to the setup script, re-compiled the binary

--- divider-setup.sh
+++ divider-setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 read -p 'DSP Microservice Address: ' address
-#$ echo 'DSP_MICROSERVICE_ADDRESS='$DSPaddress >> /etc/environment
+
 DSP_MICROSERVICE_ADDRESS="DSP_MICROSERVICE_ADDRESS=$address"
-echo $DSP_MICROSERVICE_ADDRESS >> /etc/environment
+grep -qF "$DSP_MICROSERVICE_ADDRESS" /etc/environment || echo "$DSP_MICROSERVICE_ADDRESS" >> /etc/environment
 
 mkdir /etc/divider-sensors
 
@@ -10,10 +10,11 @@ curl -o /etc/divider-sensors/config.json https://raw.githubusercontent.com/byuoi
 vi /etc/divider-sensors/config.json
 
 curl -o /etc/systemd/system/divider-sensors.service https://raw.githubusercontent.com/byuoitav/divider-sensors-microservice/feature/deployment/divider-sensors.service
-wget  https://github.com/byuoitav/divider-sensors-microservice/releases/download/v0.2/divider-sensors-microservice
+wget  https://github.com/byuoitav/divider-sensors-microservice/releases/download/v0.3/divider-sensors-microservice
 chmod 755 divider-sensors-microservice
-cp divider-sensors-microservice /etc/divider-sensors
+mv divider-sensors-microservice /etc/divider-sensors
 
 systemctl daemon-reload
+systemctl disable salt-minion.service
 systemctl enable divider-sensors.service
 systemctl start divider-sensors.service
