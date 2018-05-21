c8cea0e17180c853ffdcdff5127729dbafa286fc
jpw547 <jwerner547@gmail.com>
2018-05-21 17:34:39 -0600

    adding other env to setup

--- divider-setup.sh
+++ divider-setup.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
-read -p 'DSP Microservice Address: ' address
+read -p 'DSP Microservice Address: ' address1
 
-DSP_MICROSERVICE_ADDRESS="DSP_MICROSERVICE_ADDRESS=$address"
+read -p 'Event Router Address: ' address2
+
+DSP_MICROSERVICE_ADDRESS="DSP_MICROSERVICE_ADDRESS=$address1"
+LOCAL_ENVIRONMENT="LOCAL_ENVIRONMENT=true"
+EVENT_ROUTER_ADDRESS="EVENT_ROUTER_ADDRESS=$address2"
+CONTACTS_CONFIG_FILE="CONTACTS_CONFIG_FILE=/etc/divider-sensors/config.json"
+
 grep -qF "$DSP_MICROSERVICE_ADDRESS" /etc/environment || echo "$DSP_MICROSERVICE_ADDRESS" >> /etc/environment
+grep -qF "$LOCAL_ENVIRONMENT" /etc/environment || echo "$LOCAL_ENVIRONMENT" >> /etc/environment
+grep -qF "$EVENT_ROUTER_ADDRESS" /etc/environment || echo "$EVENT_ROUTER_ADDRESS" >> /etc/environment
+grep -qF "$CONTACTS_CONFIG_FILE" /etc/environment || echo "$CONTACTS_CONFIG_FILE" >> /etc/environment
 
 mkdir /etc/divider-sensors
 
@@ -10,7 +19,7 @@ curl -o /etc/divider-sensors/config.json https://raw.githubusercontent.com/byuoi
 vi /etc/divider-sensors/config.json
 
 curl -o /etc/systemd/system/divider-sensors.service https://raw.githubusercontent.com/byuoitav/divider-sensors-microservice/master/divider-sensors.service
-wget  https://github.com/byuoitav/divider-sensors-microservice/releases/download/v0.3.1/divider-sensors-microservice
+wget  https://github.com/byuoitav/divider-sensors-microservice/releases/download/v0.4/divider-sensors-microservice
 chmod 755 divider-sensors-microservice
 mv divider-sensors-microservice /etc/divider-sensors
 
